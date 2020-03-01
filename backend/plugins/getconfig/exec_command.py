eaa8e4375fffd13cbd696f4926aa6e4fd69a8c42
tbotnz <tonynealon1989@gmail.com>
2020-03-01 23:40:03 +0000

    fix driver handling excp

--- backend/plugins/getconfig/exec_command.py
+++ backend/plugins/getconfig/exec_command.py
@@ -9,14 +9,20 @@ def exec_command(**kwargs):
     else:
         commandlst = command
     if lib == "netmiko":
-        netmik = netmko(**kwargs)
-        sesh = netmik.connect()
-        result = netmik.sendcommand(sesh,commandlst)
-        netmik.logout(sesh)
-        return result
+        try:
+            netmik = netmko(**kwargs)
+            sesh = netmik.connect()
+            result = netmik.sendcommand(sesh,commandlst)
+            netmik.logout(sesh)
+            return result
+        except Exception as e:
+            return e
     elif lib == "napalm":
-        napl = naplm(**kwargs)
-        sesh = napl.connect()
-        result = napl.sendcommand(sesh,commandlst)
-        napl.logout(sesh)
-        return result
+        try:
+            napl = naplm(**kwargs)
+            sesh = napl.connect()
+            result = napl.sendcommand(sesh,commandlst)
+            napl.logout(sesh)
+            return result
+        except Exception as e:
+            return e
--- backend/plugins/setconfig/exec_config.py
+++ backend/plugins/setconfig/exec_config.py
@@ -5,14 +5,20 @@ def exec_config(**kwargs):
     lib = kwargs.get("library", False)
     config = kwargs.get("config", False)
     if lib == "netmiko":
-        netmik = netmko(**kwargs)
-        sesh = netmik.connect()
-        result = netmik.config(sesh,config)
-        netmik.logout(sesh)
-        return result
+        try:
+            netmik = netmko(**kwargs)
+            sesh = netmik.connect()
+            result = netmik.config(sesh,config)
+            netmik.logout(sesh)
+            return result
+        except Exception as e:
+            return e
     elif lib == "napalm":
-        napl = naplm(**kwargs)
-        sesh = napl.connect()
-        result = napl.config(sesh,config)
-        napl.logout(sesh)
-        return result
+        try:
+            napl = naplm(**kwargs)
+            sesh = napl.connect()
+            result = napl.config(sesh,config)
+            napl.logout(sesh)
+            return result
+        except Exception as e:
+            return e
