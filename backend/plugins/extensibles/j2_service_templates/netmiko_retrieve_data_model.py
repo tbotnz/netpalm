af344c238d721e9f1d1bba63b5d942e560bae5be
ndo@$(hostname) <yo@ndo.dev>
2020-08-18 00:33:20 +0200

    update: QueueStrategy import

--- backend/plugins/extensibles/j2_service_templates/netmiko_retrieve_data_model.py
+++ backend/plugins/extensibles/j2_service_templates/netmiko_retrieve_data_model.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 from typing import Dict, FrozenSet, List, Optional, Sequence, Set, Tuple, Union
 
 from backend.core.models.service import service_lifecycle
-from backend.core.models.models import queue_strat
+from backend.core.models.models import QueueStrategy
 
 class netmiko_retrieve_data_model_args(BaseModel):
     hosts: list
@@ -14,7 +14,7 @@ class netmiko_retrieve_data_model_args(BaseModel):
 class netmiko_retrieve_data_model(BaseModel):
     operation: service_lifecycle
     args: netmiko_retrieve_data_model_args
-    queue_strategy: Optional[queue_strat] = None
+    queue_strategy: Optional[QueueStrategy] = None
 
     class Config:
         schema_extra = {
