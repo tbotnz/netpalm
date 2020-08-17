83011052d82bd4a43b4dffb436f913fc286c363a
ndo@$(hostname) <yo@ndo.dev>
2020-08-17 23:39:37 +0200

    update: formatting

--- backend/plugins/extensibles/j2_service_templates/netmiko_retrieve_data_model.py
+++ backend/plugins/extensibles/j2_service_templates/netmiko_retrieve_data_model.py
@@ -5,7 +5,6 @@ from backend.core.models.service import service_lifecycle
 from backend.core.models.models import queue_strat
 
 class netmiko_retrieve_data_model_args(BaseModel):
-    # your model goes here!
     hosts: list
     username: str
     password: str
@@ -13,20 +12,18 @@ class netmiko_retrieve_data_model_args(BaseModel):
     command: str
 
 class netmiko_retrieve_data_model(BaseModel):
-    # this class MUST match the filename & the filename must be formatted $servicetemplatename_model.py
     operation: service_lifecycle
     args: netmiko_retrieve_data_model_args
     queue_strategy: Optional[queue_strat] = None
 
     class Config:
-        # add an example payload under the "example" dict 
         schema_extra = {
             "example": {
                 "operation": "retrieve",
                 "args":{
-                    "hosts":["10.0.2.25","10.0.2.23"],
-                    "username": "{{device_username}}",
-                    "password": "{{device_password}}",
+                    "hosts": ["10.0.2.25","10.0.2.23"],
+                    "username": "{{password}}",
+                    "password": "{{username}}",
                     "driver":   "{{driver}}",
                     "command":  "{{command}}",
                 }
