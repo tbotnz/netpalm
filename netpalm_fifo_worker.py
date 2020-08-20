3be74d14ce940fbd97c9e0c62168a9f3a5630fa0
tbotnz <tonynealon1989@gmail.com>
2020-08-20 10:36:45 +0000

    scale_in_pinned

--- netpalm_fifo_worker.py
+++ netpalm_fifo_worker.py
@@ -1,3 +1,4 @@
+import uuid
 from multiprocessing import Process
 
 from redis import Redis
@@ -9,11 +10,13 @@ from netpalm_worker_common import start_broadcast_listener_process
 config.setup_logging()
 
 
-def fifo_worker(queue):
+def fifo_worker(queue, counter):
     try:
         with Connection(Redis(host=config.redis_server, port=config.redis_port, password=config.redis_key)):
             q = Queue(queue)
-            worker = Worker(q)
+            u_uid = uuid.uuid4()
+            worker_name = f"{queue}_{counter}_{u_uid}"
+            worker = Worker(q, name=worker_name)
             worker.work()
     except Exception as e:
         return e
@@ -21,7 +24,7 @@ def fifo_worker(queue):
 def fifo_worker_constructor(queue):
     start_broadcast_listener_process()
     for i in range(config.fifo_process_per_node):
-        p = Process(target=fifo_worker, args=(queue,))
+        p = Process(target=fifo_worker, args=(queue,i,))
         p.start()
 
 fifo_worker_constructor(config.redis_fifo_q)
