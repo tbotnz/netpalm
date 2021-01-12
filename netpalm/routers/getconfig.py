4c32b5f1cf60215850652a9969221fed42eca36b
Will George <wrgeorge1983@gmail.com>
2021-01-12 10:39:16 -0500

    Add alias "get" for all "getconfig" routes
    If we point documentation to this, it should be more clear that this is *the* method for retreiving both operational and config data.

--- netpalm/routers/getconfig.py
+++ netpalm/routers/getconfig.py
@@ -30,6 +30,7 @@ def _get_config(getcfg: GetConfig, library: str = None):
 
 # read config
 @router.post("/getconfig", response_model=Response, status_code=201)
+@router.post("/get", response_model=Response, status_code=201)
 @error_handle_w_cache
 def get_config(getcfg: GetConfig):
     return _get_config(getcfg)
@@ -37,6 +38,7 @@ def get_config(getcfg: GetConfig):
 
 # read config
 @router.post("/getconfig/netmiko", response_model=Response, status_code=201)
+@router.post("/get/netmiko", response_model=Response, status_code=201)
 @error_handle_w_cache
 def get_config_netmiko(getcfg: NetmikoGetConfig):
     return _get_config(getcfg, library="netmiko")
@@ -44,6 +46,7 @@ def get_config_netmiko(getcfg: NetmikoGetConfig):
 
 # read config
 @router.post("/getconfig/napalm", response_model=Response, status_code=201)
+@router.post("/get/napalm", response_model=Response, status_code=201)
 @error_handle_w_cache
 def get_config_napalm(getcfg: NapalmGetConfig):
     return _get_config(getcfg, library="napalm")
@@ -51,6 +54,7 @@ def get_config_napalm(getcfg: NapalmGetConfig):
 
 # read config
 @router.post("/getconfig/puresnmp", response_model=Response, status_code=201)
+@router.post("/get/puresnmp", response_model=Response, status_code=201)
 @error_handle_w_cache
 def get_config_puresnmp(getcfg: PureSNMPGetConfig):
     return _get_config(getcfg, library="puresnmp")
@@ -58,6 +62,7 @@ def get_config_puresnmp(getcfg: PureSNMPGetConfig):
 
 # read config
 @router.post("/getconfig/ncclient", response_model=Response, status_code=201)
+@router.post("/get/ncclient", response_model=Response, status_code=201)
 @error_handle_w_cache
 def get_config_ncclient(getcfg: NcclientGetConfig):
     return _get_config(getcfg, library="ncclient")
@@ -67,6 +72,9 @@ def get_config_ncclient(getcfg: NcclientGetConfig):
 # Certain device types dont have rpc methods defined in ncclient.
 # This is a work around for that.
 @router.post("/getconfig/ncclient/get",
+             response_model=Response,
+             status_code=201)
+@router.post("/get/ncclient/get",
              response_model=Response,
              status_code=201)
 @error_handle_w_cache
@@ -81,6 +89,7 @@ def ncclient_get(getcfg: NcclientGet, library: str = "ncclient"):
 
 # read config
 @router.post("/getconfig/restconf", response_model=Response, status_code=201)
+@router.post("/get/restconf", response_model=Response, status_code=201)
 @error_handle_w_cache
 def get_config_restconf(getcfg: Restconf):
     return _get_config(getcfg, library="restconf")
