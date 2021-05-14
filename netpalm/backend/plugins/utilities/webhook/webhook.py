eb98b3590fdbee192d109be84769cbddbb382948
tbotnz <tonynealon1989@gmail.com>
2021-05-14 23:38:03 +1200

    Update webhook.py

--- netpalm/backend/plugins/utilities/webhook/webhook.py
+++ netpalm/backend/plugins/utilities/webhook/webhook.py
@@ -1,9 +1,11 @@
 import importlib
 import json
+import logging
 
 from netpalm.backend.core.confload.confload import config
 from netpalm.backend.plugins.utilities.jinja2.j2 import render_j2template
 
+log = logging.getLogger(__name__)
 
 class webhook_runner:
 
@@ -18,11 +20,14 @@ class webhook_runner:
 
     def webhook_exec(self, job_data):
         try:
+            log.info(f"webhook_exec: importing f"{self.webhook_name}")
             module = importlib.import_module(self.webhook_name)
             run_whook = getattr(module, "run_webhook")
             job_data["webhook_args"] = self.webhook_args
             whook_data = job_data
+            log.info(f"webhook_exec: webhook data loaded f"{whook_data}")
             if self.webhook_j2_name:
+                log.info(f"webhook_exec: rendering webhook j2 template f"{self.webhook_j2_name}")
                 res = render_j2template(
                                         self.webhook_j2_name,
                                         template_type="webhook",
@@ -34,6 +39,7 @@ class webhook_runner:
             res = run_whook(payload=whook_data)
             return res
         except Exception as e:
+            log.error(f"webhook_exec: webhook run failure f"{e}")
             return e
 
 
