b6a2ddd218cff205f2cd043e1ede37dd023482e7
Tishey, John <john.tishey@windstream.com>
2021-10-29 15:41:08 -0400

    Undo the extra changes my formatter did

--- netpalm/backend/plugins/calls/scriptrunner/script.py
+++ netpalm/backend/plugins/calls/scriptrunner/script.py
@@ -1,10 +1,7 @@
 import importlib
 
 from netpalm.backend.core.confload.confload import config
-from netpalm.backend.core.utilities.rediz_meta import (
-    render_netpalm_payload,
-    write_mandatory_meta,
-)
+from netpalm.backend.core.utilities.rediz_meta import render_netpalm_payload, write_mandatory_meta
 from netpalm.backend.core.utilities.rediz_meta import write_meta_error
 from netpalm.backend.plugins.utilities.webhook.webhook import exec_webhook_func
 
@@ -12,10 +9,10 @@ from netpalm.backend.plugins.utilities.webhook.webhook import exec_webhook_func
 class script_kiddy:
     def __init__(self, **kwargs):
         self.scrp_path = config.custom_scripts
-        self.kwarg = kwargs.get("kwargs", False)
-        self.arg = self.kwarg.get("args", False)
-        self.script = self.kwarg.get("script", False)
-        self.script_name = self.scrp_path.replace("/", ".") + self.script
+        self.kwarg = kwargs.get('kwargs', False)
+        self.arg = self.kwarg.get('args', False)
+        self.script = self.kwarg.get('script', False)
+        self.script_name = self.scrp_path.replace('/', '.') + self.script
 
     def s_exec(self):
         module = importlib.import_module(self.script_name)
--- tests/integration/test_script.py
+++ tests/integration/test_script.py
@@ -8,14 +8,24 @@ helper = NetpalmTestHelper()
 
 @pytest.mark.script
 def test_exec_script():
-    pl = {"script": "hello_world", "args": {"hello": "world"}}
+    pl = {
+        "script":"hello_world",
+        "args":{
+            "hello":"world"
+        }
+    }
     res = helper.post_and_check("/script", pl)
     assert res == "world"
 
 
 @pytest.mark.script
 def test_exec_script_failure():
-    pl = {"script": "hello_world", "args": {"bad": "args"}}
+    pl = {
+        "script":"hello_world",
+        "args":{
+            "bad":"args"
+        }
+    }
     res = helper.post_and_check("/script", pl)
     res2 = helper.post_and_check_errors("/script", pl)
     assert res is None
