ec82dd8e63546c44a2ba3377bf8711fa01fff926
Tishey, John <john.tishey@windstream.com>
2021-10-29 15:24:54 -0400

    Update test and sample scripts for failures #169

--- tests/integration/test_script.py
+++ tests/integration/test_script.py
@@ -5,14 +5,18 @@ from tests.integration.helper import NetpalmTestHelper
 
 helper = NetpalmTestHelper()
 
+
 @pytest.mark.script
 def test_exec_script():
-    pl = {
-        "script":"hello_world",
-        "args":{
-            "hello":"world"
-        }
-    }
-    res = helper.post_and_check('/script',pl)
+    pl = {"script": "hello_world", "args": {"hello": "world"}}
+    res = helper.post_and_check("/script", pl)
     assert res == "world"
 
+
+@pytest.mark.script
+def test_exec_script_failure():
+    pl = {"script": "hello_world", "args": {"bad": "args"}}
+    res = helper.post_and_check("/script", pl)
+    res2 = helper.post_and_check_errors("/script", pl)
+    assert res is None
+    assert res2 == ["Required args: 'hello'"]
